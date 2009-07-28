d33d2e180da06313d9526b95ea8f05d8b124c9f1
nielinjie <nielinjie@gmail.com>
2009-07-28 23:11:32 +0800

    exploreing...

--- test/unit/PluginTests.groovy
+++ test/unit/PluginTests.groovy
@@ -3,9 +3,10 @@ import static org.junit.Assert.*
 import static org.hamcrest.CoreMatchers.*
 import static org.junit.Assert.assertThat as at
 
+import org.java.plugin.*
 import org.java.plugin.boot.*
 import org.java.plugin.util.*
-
+import tagging.*
 class PluginTests{
     @Test void boot(){
         String[] args={}
@@ -14,5 +15,17 @@ class PluginTests{
 		prop.put('org.java.plugin.boot.applicationPlugin','tagging.taggingManager')
         def app=Boot.boot( prop,false,Boot.BOOT_MODE_SHELL ,new BootErrorHandlerConsole(),args)
         at app,is(not(null))
+		//def manager= PluginManager.lookup(tagging.TaggingManagerPlugin.class);
+        at app.manager ,is (not (null))
+		def extensions =
+            app.manager.registry.getExtensionPoint(
+                'tagging.taggingManager', 'Meta').connectedExtensions
+		at extensions.size(),is(1)
+		extensions.each{
+        	def className=it.getParameter('class').valueAsString()
+        	at className,is(not(null))
+        }
+        
+		
     }
 }
